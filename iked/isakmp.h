9c8fe238d27659552e0adabc33b6b08e81ac97ce
ssszcmawo <belan2470@gmail.com>
2026-06-04 21:02:51 +0200

    add isakmp_frag_hdr structure to properly support IKEv1 fragmentation

--- iked/isakmp.h
+++ iked/isakmp.h
@@ -165,6 +165,15 @@ struct isakmp_gen {
 	uint16_t len;		/* Payload Length */
 } __attribute__ ((__packed__));
 
+struct isakmp_frag_hdr {
+	struct isakmp_gen h;
+	uint8_t frag_id;
+	uint8_t flags;
+	uint16_t frag_no;
+} __attribute__ ((__packed__));
+
+#define ISAKMP_FRAG_MORE	0x01
+
 /* 3.3 Data Attributes
          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
